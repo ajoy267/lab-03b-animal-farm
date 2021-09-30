f123b406cf1d472184165a5e642d690c493a93f3
ajoy267 <andrew.joy12@hotmail.com>
2021-09-29 17:04:40 -0700

    added the keydown eventlistener for each of the animals

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ catImg.addEventListener('click', ()=>{
 dogImg.addEventListener('click', ()=>{
     dogSound.play();
 });
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'h'){
+        horseSound.play();
+    } if (event.key === 'c'){
+        catSound.play();
+    } if (event.key === 'd'){
+        dogSound.play();
+    }
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
